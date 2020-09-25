8eb7b5911d73829ccdd744dbd0a7a734cb2b0dd8
Venkata Chenchaiah Kotta <chenchuhere@gmail.com>
2020-09-25 16:57:23 -0400

    Update createpage.java
    updated the qa ip address - 34.122.139.182

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://35.225.162.173:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://34.122.139.182:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
