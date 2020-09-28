b012aa7b6ac5de60c5e1c6831b45773ca5103daf
Venkata Chenchaiah Kotta <chenchuhere@gmail.com>
2020-09-28 11:34:09 -0400

    Update cancelpage.java
    35.226.22.28

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://35.238.243.0:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://35.226.22.28:5432/postgres";
 					String username="postgres";
 					String password="password";
 					
